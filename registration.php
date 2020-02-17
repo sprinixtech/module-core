97a39e1f13dd6a03af5f9a36f3b7593a60140f38
Gulshan Maurya <gulshan.4dream@gmail.com>
2020-02-17 12:58:37 +0530

    Init Core Module Repo

---
+++ registration.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @author gulshan maurya
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'Sprinix_Core',
+    __DIR__
+);
